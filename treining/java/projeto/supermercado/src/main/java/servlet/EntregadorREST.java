ddab7b68330f8ce36b534290c2039dfc5b4800a3
Ana Júlia Moreira Vieira <a.jlmoreira7@gmail.com>
2021-10-19 23:33:20 -0300

    http servlet - incompleto

--- treining/java/projeto/supermercado/src/main/java/servlet/EntregadorREST.java
+++ treining/java/projeto/supermercado/src/main/java/servlet/EntregadorREST.java
@@ -2,17 +2,19 @@ package servlet;
 
 import java.io.IOException;
 import org.json.JSONArray;
-import controller.ClienteProcess;
+
+import controller.EntregadorProcess;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Cliente;
+import model.Entregador;
 
-@WebServlet("/clientes")
-public class ClienteREST extends HttpServlet {
-	
+
+@WebServlet("/entregadores")
+public class EntregadorREST extends HttpServlet{
+
 	private static final long serialVersionUID = 1L;
 	
 	// read
@@ -22,20 +24,20 @@ public class ClienteREST extends HttpServlet {
 		resp.setContentType("application/json"); // configura a resposta no formato json
 		resp.setCharacterEncoding("utf8"); // configuração do charset
 		
-		ClienteProcess.testes(); //abrindo e lendo dados do banco
+		EntregadorProcess.testes();
 		
 		// recebendo dados por parâmetro
-		String id = req.getParameter("idCliente");
-		if(id != null) { // verifica se chegou o parâmetro id
-			if(ClienteProcess.clientes.contains(new Cliente(id))) { // se caso tiver o id na lista
-				int ind = ClienteProcess.clientes.indexOf(new Cliente(id)); // obtem o indice
-				resp.getWriter().print(ClienteProcess.clientes.get(ind).toJSON()); // nos da a resposta em formato json
+		String id = req.getParameter("idEntregador");
+		if(id != null) {
+			if(EntregadorProcess.entregadores.contains(new Entregador(id))) {
+				int ind = EntregadorProcess.entregadores.indexOf(new Entregador(id)); // obtem o indice
+				resp.getWriter().print(EntregadorProcess.entregadores.get(ind).toJSON()); // nos da a resposta em formato json
 			}else {
-				resp.setStatus(HttpServletResponse.SC_NOT_FOUND); // caso não tenha, mensagem de erro aparecerá
+				resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			}
 		}else {
 			JSONArray ja = new JSONArray(); // armazena cada objeto json
-			ClienteProcess.clientes.forEach(c -> ja.put(c.toJSON())); // percorre preenchendo o vetor com dados da lista
+			EntregadorProcess.entregadores.forEach(en -> ja.put(en.toJSON())); // percorre preenchendo o vetor com dados da lista
 			resp.getWriter().print(ja); // resposta, mostra o vetor json
 		}
 	}
