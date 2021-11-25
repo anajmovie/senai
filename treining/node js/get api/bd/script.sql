36c6e34725dd683e373358a4871abd3cd974327f
Ana Júlia Moreira Vieira <a.jlmoreira7@gmail.com>
2021-11-25 10:56:03 -0200

    get api node

---
+++ treining/node js/get api/bd/script.sql
@@ -0,0 +1,21 @@
+drop database if exists academia;
+create database academia;
+use academia;
+
+create table alunos (
+id integer not null primary key auto_increment,
+nome varchar(255) not null,
+peso decimal(5,2) not null,
+altura decimal(3,2) not null,
+nascimento date not null
+);
+
+-- importando dados de academia.csv --
+load data INFILE 'C:/Users/Desenvolvimento/Desktop/2des/pwbe/node/get api/bd/academia.csv'
+into table alunos
+FIELDS TERMINATED by ';'
+ENCLOSED by '"'
+LINES TERMINATED by '\r\n'
+IGNORE 1 rows;
+
+select * from alunos;
