cba6f1c6ee1ee3a97e96caae8be4df66c14463c7
Ana Júlia Moreira Vieira <a.jlmoreira7@gmail.com>
2021-11-26 10:36:31 -0200

    crud mvc - exemplo

--- treining/node js/mvc academia/controll/aluno_controll.js
+++ treining/node js/mvc academia/controll/aluno_controll.js
@@ -1,6 +1,12 @@
 const { con } = require("./mysql_controll.js")
 const model = require('../model/aluno_model.js')
 // função de resposta
+const getAll = (req, res) => {
+    let string = 'select * from alunos'
+    con.query(string, (err, result) => {
+        res.json(result)
+    })
+}
 const getId = (req, res) => {
     let string = 'select * from alunos where id = '+ req.params.id;
     con.query(string, (err, result) => {
@@ -32,8 +38,53 @@ const imcStatus = (req, res) => {
     }) 
 }
 
+const addAluno = (req, res) => {
+    let body = req.body
+    let string = 'insert into alunos(nome, peso, altura, nascimento) values (\'' + body.nome + '\',' + body.peso + ',' + body.altura + ',\'' + body.nascimento + '\')'
+    con.query(string, (err, result) => {
+        if(err != null){
+            res.status(400).end()
+        } else {
+            res.status(200).end()
+        }
+    })
+}
+
+const delAluno = (req, res) => {
+    let string = 'delete from alunos where id = ' + req.params.id
+    con.query(string, (err, result) => {
+        if(result.affectedRows == 0){
+            res.send(400).end()
+        } else {
+            res.send(200).end()
+        }
+    })
+}
+
+const updAluno = (req, res) => {
+    let nome = '\''+req.body.nome+'\''
+    let peso = req.body.peso
+    let altura = req.body.altura
+    let nascimento = '\''+req.body.nascimento+'\''
+    let id = req.body.id
+    let string = `update alunos set nome = ${nome}, peso = ${peso}, altura = ${altura}, nascimento = ${nascimento} where id = ${id}`
+    con.query(string, (err, result) => {
+        if(result.affectedRows == 1){
+            con.query('select * from alunos where id = '+id, (err, result) => {
+                res.json(result)
+            })
+        } else{
+                res.send(400).end()
+        }
+    })
+}
+
 module.exports = {
+    getAll,
     getId,
     imcId,
-    imcStatus
+    imcStatus,
+    addAluno,
+    delAluno,
+    updAluno
 }
--- treining/node js/mvc academia/routes/aluno_route.js
+++ treining/node js/mvc academia/routes/aluno_route.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const aluno_controll = require('../controll/aluno_controll.js')
-router.get('/api/:id', aluno_controll.getId)
-router.get('/imc/:id', aluno_controll.imcId)
-router.get('/alunos/imc', aluno_controll.imcStatus)
+router.get('/academia/alunos', aluno_controll.getAll)
+router.get('/academia/aluno/:id', aluno_controll.getId)
+router.get('/academia/imc/:id', aluno_controll.imcId)
+router.get('/academia/status', aluno_controll.imcStatus)
+router.post('/academia/create', aluno_controll.addAluno)
+router.delete('/academia/delete/:id', aluno_controll.delAluno)
+router.put('/academia/update', aluno_controll.updAluno)
 
 module.exports = router
